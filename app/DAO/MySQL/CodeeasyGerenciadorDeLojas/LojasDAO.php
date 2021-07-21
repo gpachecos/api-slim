8ddf8e29dce844ecd0a4bd065a08cb13435b9c7c
gpachecos <guilherme.pacheco30@gmail.com>
2021-07-20 21:11:01 -0300

    api slim

---
+++ app/DAO/MySQL/CodeeasyGerenciadorDeLojas/LojasDAO.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\DAO\MySQL\CodeeasyGerenciadorDeLojas;
+
+use App\Models\MySQL\CodeeasyGerenciadorDeLojas\LojaModel;
+
+class LojasDAO extends Conexao
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function getAllLojas(): array
+    {
+        $lojas = $this->pdo
+            ->query('Select
+                        id,
+                        nome, 
+                        telefone,
+                        endereco
+                     From lojas;')
+            ->fetchAll(\PDO::FETCH_ASSOC);
+        
+        return $lojas;
+    }
+
+    public function insertLoja(LojaModel $loja): void
+    {
+        $statement = $this->pdo
+            ->prepare('INSERT INTO lojas VALUES(
+                null,
+                :nome,
+                :telefone,
+                :endereco
+            );');
+            $statement->execute([
+                'nome' => $loja->getNome(),
+                'telefone' => $loja->getTelefone(),
+                'endereco' => $loja->getEndereco()
+            ]);
+    }
+
+    public function updateLoja(LojaModel $loja): void
+    {
+        $statement = $this->pdo
+            ->prepare('UPDATE lojas 
+                          SET nome = :nome,
+                              telefone = :telefone,
+                              endereco = :endereco
+                         where id = :id
+                    ;');
+            $statement->execute([
+                'nome' =>$loja->getNome(),
+                'telefone' =>$loja->getTelefone(),
+                'endereco' =>$loja->getEndereco(),
+                'id' =>$loja->getId()
+            ]);
+
+    }
+
+    public function deleteLoja(int $id): void
+    {
+        $statement = $this->pdo
+            ->prepare('DELETE FROM produtos WHERE loja_id = :id;
+                       DELETE FROM lojas WHERE id = :id;
+                    ');
+            $statement->execute([
+                'id' =>$id
+            ]);
+    }
+}
---
+++ routes/index.php
@@ -0,0 +1,27 @@
+<?php
+
+use function src\{
+    slimConfiguration,
+    basicAuth
+};
+use App\Controllers\ProdutoController;
+use App\Controllers\LojaController;
+
+$app = new \Slim\App(slimConfiguration());
+
+// =======================================
+
+$app->group('',function() use ($app) {
+    $app->get('/loja', LojaController::class . ':getLojas');
+    $app->post('/loja', LojaController::class . ':insertLoja');
+    $app->put('/loja', LojaController::class . ':updateLoja');
+    $app->delete('/loja', LojaController::class . ':deleteLoja');
+
+    $app->get('/produto', ProdutoController::class . ':getProdutos');
+    $app->post('/produto', ProdutoController::class . ':insertProduto');
+    $app->put('/produto', ProdutoController::class . ':updateProduto');
+    $app->delete('/produto', ProdutoController::class . ':deleteProduto');
+})->add(basicAuth());
+// =======================================
+
+$app->run();
